274192e35f6017a66027c46f537bfa4649db6a35
Emiliano Alonzo <emialonzo@gmail.com>
2018-05-08 16:04:15 -0500

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+import './Header.css';
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar className="navbarr" color="light" light expand="md">
+                    <NavbarBrand href="/">Lector de notas de kindle</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/emialonzo">Mi GitHub</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                <a href="https://github.com/emialonzo/kindlenotesviewer">
+                    <img style={{ position: "absolute", top: 0, right: 0, border: 0 }} src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub" />
+                </a>
+            </div>
+        );
+    }
+}
